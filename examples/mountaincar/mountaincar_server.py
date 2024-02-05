0ff86501d6c1ac48010ff08137302590fc484ed5
chasemcd <chasecmcdonald@gmail.com>
2024-02-05 00:53:56 -0500

    remove extra line

--- examples/mountaincar/mountaincar_server.py
+++ examples/mountaincar/mountaincar_server.py
@@ -35,7 +35,6 @@ config = (
     .environment(env_creator=env_creator, env_name="MountainCar-v0")
     .rendering(fps=30, env_to_state_fn=mountaincar_utils.mountaincar_to_render_state)
     .gameplay(
-        human_id="agent-0",
         default_action=NOOP_ACTION,
         action_mapping=action_mapping,
         num_episodes=2,
