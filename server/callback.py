04039f97d46c41b9387b80016208c8bbba8330b7
chasemcd <chasecmcdonald@gmail.com>
2024-02-11 18:38:14 -0500

    pass game to callback

--- server/callback.py
+++ server/callback.py
@@ -1,30 +1,33 @@
+from server import remote_game
+
+
 class GameCallback:
-    def on_episode_start(self):
+    def on_episode_start(self, remote_game: remote_game.RemoteGame):
         pass
 
-    def on_episode_end(self):
+    def on_episode_end(self, remote_game: remote_game.RemoteGame):
         pass
 
-    def on_game_tick(self):
+    def on_game_tick(self, remote_game: remote_game.RemoteGame):
         pass
 
-    def on_graphics_start(self):
+    def on_graphics_start(self, remote_game: remote_game.RemoteGame):
         pass
 
-    def on_graphics_end(self):
+    def on_graphics_end(self, remote_game: remote_game.RemoteGame):
         pass
 
-    def on_waitroom_start(self):
+    def on_waitroom_start(self, remote_game: remote_game.RemoteGame):
         pass
 
-    def on_waitroom_join(self):
+    def on_waitroom_join(self, remote_game: remote_game.RemoteGame):
         pass
 
-    def on_waitroom_end(self):
+    def on_waitroom_end(self, remote_game: remote_game.RemoteGame):
         pass
 
-    def on_waitroom_timeout(self):
+    def on_waitroom_timeout(self, remote_game: remote_game.RemoteGame):
         pass
 
-    def on_game_end(self):
+    def on_game_end(self, remote_game: remote_game.RemoteGame):
         pass
