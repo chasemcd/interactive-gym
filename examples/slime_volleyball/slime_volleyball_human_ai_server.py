140de4b08246bc484d42e10fe89590756e7282c4
Chase M. <chasecmcdonald@gmail.com>
2024-02-26 05:39:02 +0000

    bugfix

--- examples/slime_volleyball/slime_volleyball_human_ai_server.py
+++ examples/slime_volleyball/slime_volleyball_human_ai_server.py
@@ -90,10 +90,6 @@ config = (
         final_page_text="Thanks for playing, you will be redirected shortly...",
         redirect_url="https://cmu.ca1.qualtrics.com/jfe/form/SV_b7yGut4znAui0hE",
         waitroom_timeout=120_000,  # 2 minutes in waitroom
-        waitroom_time_randomization_interval_s=(
-            5,
-            25,
-        ),  # fake waitroom of 5 to 25 seconds
     )
 )
 
