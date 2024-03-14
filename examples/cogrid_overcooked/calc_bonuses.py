6c78f90021e974bcec7a11ebc3f130168af7a732
chasemcd <chasecmcdonald@gmail.com>
2024-03-14 19:26:10 +0000

    calculate bonuses that match mturk pattern

--- examples/cogrid_overcooked/calc_bonuses.py
+++ examples/cogrid_overcooked/calc_bonuses.py
@@ -2,7 +2,7 @@ import os
 
 import pandas as pd
 
-DATA_PATH = "data/overcooked/03011/overcooked"
+DATA_PATH = "data/overcooked/0311/overcooked"
 
 
 def read_files(data_path: str) -> pd.DataFrame:
@@ -41,13 +41,21 @@ def calc_bonuses(df: pd.DataFrame) -> None:
     df["action_is_noop"] = df["agent-0_action"] == 6.0
     df["max_episode_num"] = df.groupby("game_uuid")["episode_num"].transform("max")
     # df = df[df["agent-0_identifier"] == "29255f7c-19f0-4fd4-bf63-d3f926ca8915"]
-    df = df[df.max_episode_num >= 20]
-    df["agent-0_doc_in_focus"] = df["agent-0_doc_in_focus"].astype(int)
+    # df = df[df.max_episode_num >= 20]
+    df["agent-0_doc_in_focus"] = (
+        df["agent-0_doc_in_focus"].map(lambda x: x if not pd.isna(x) else 0).astype(int)
+    )
 
     df.rename(
         columns={"agent-0_identifier": "mturk_id", "agent-0_reward": "score"},
         inplace=True,
     )
+    df = df[
+        df.mturk_id.apply(
+            lambda x: len(str(x)) in [12, 13, 14, 15] and str(x)[0] == "A"
+        )
+    ]
+
     df["score"] = df["score"].map(lambda x: x if x == 1 else 0)
 
     result = df.groupby("mturk_id").sum(numeric_only=True).reset_index()
