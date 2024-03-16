79b22647cc593059db820cfdb041967c642812c0
chasemcd <chasecmcdonald@gmail.com>
2024-03-16 11:16:24 +0000

    200 max ping

--- examples/slime_volleyball/slime_volleyball_human_human_server.py
+++ examples/slime_volleyball/slime_volleyball_human_human_server.py
@@ -76,7 +76,7 @@ config = (
         num_episodes=30,
         callback=slime_volleyball_callback.SlimeVolleyballCallback(),
     )
-    .hosting(port=5704, host="0.0.0.0", max_concurrent_games=100, max_ping=85)
+    .hosting(port=5704, host="0.0.0.0", max_concurrent_games=100, max_ping=200)
     .user_experience(
         page_title="Slime Volleyball",
         welcome_header_text="Slime Volleyball",
