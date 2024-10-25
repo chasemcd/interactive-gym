42c4e9dda5175e697521bb153bf16ab256137761
Chase McDonald <chasecmcdonald@gmail.com>
2024-10-25 10:14:53 -0400

    port to 5704

--- interactive_gym/examples/cogrid/overcooked_human_ai_controllable_ibc.py
+++ interactive_gym/examples/cogrid/overcooked_human_ai_controllable_ibc.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
     experiment_config = (
         experiment_config.ExperimentConfig()
         .experiment(stager=stager, experiment_id="overcooked_controllable")
-        .hosting(port=5702, host="0.0.0.0")
+        .hosting(port=5704, host="0.0.0.0")
     )
 
     app.run(experiment_config)
