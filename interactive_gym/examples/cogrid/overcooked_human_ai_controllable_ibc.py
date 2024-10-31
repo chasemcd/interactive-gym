f38f9b22a47a20c9f3be8e47bd46aa198102c56b
Chase McDonald <chasecmcdonald@gmail.com>
2024-10-31 00:18:43 -0400

    enable all scenes

--- interactive_gym/examples/cogrid/overcooked_human_ai_controllable_ibc.py
+++ interactive_gym/examples/cogrid/overcooked_human_ai_controllable_ibc.py
@@ -19,10 +19,10 @@ from interactive_gym.configurations import experiment_config
 stager = stager.Stager(
     scenes=[
         controllable_scenes.start_scene,
-        # scenes.tutorial_gym_scene,
-        # controllable_scenes.tutorial_with_bot_scene,
-        # controllable_scenes.control_tutorial_scene,
-        # controllable_scenes.end_tutorial_static_scene,
+        scenes.tutorial_gym_scene,
+        controllable_scenes.tutorial_with_bot_scene,
+        controllable_scenes.control_tutorial_scene,
+        controllable_scenes.end_tutorial_static_scene,
         controllable_scenes.cramped_room_scenes,
         controllable_scenes.end_scene,
     ]
